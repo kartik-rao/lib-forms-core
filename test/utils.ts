525fa92f2e764bf068a77be99e12bff6b018502c
Kartik Rao <kartik.rao@adinfinity.com>
2019-02-23 17:06:43 +1100

    Added helper to generate unique ids

--- test/utils.ts
+++ test/utils.ts
@@ -5,3 +5,7 @@ export const jsxToHTML = require('react-dom/server').renderToStaticMarkup
 export const printPrettyHtml = (html)=> {
     console.warn("\n"+ beautifyHtml( html, {"indent_size":2} ).replace(/class=/g, "className=") )
 }
+
+export const genElementId = (prefix: string) => {
+    return `${prefix}-${Math.ceil(Math.random() * 1e6)}`
+}
