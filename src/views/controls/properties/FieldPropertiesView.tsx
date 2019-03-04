a9b65b25a06cfd7318c3715f5d9bf81f2e27e7fa
Kartik Rao <kartik.rao@adinfinity.com>
2019-03-04 20:52:17 +1100

    Made property update two step

--- src/views/controls/properties/FieldPropertiesView.tsx
+++ src/views/controls/properties/FieldPropertiesView.tsx
@@ -1,40 +1,54 @@
 import { observer } from "mobx-react";
+import {toJS} from "mobx";
+
 import * as React from "react";
-import { Form, Input, Select, Row, Col} from "antd";
+import { Form, Input, Select, Row, Col, Card, Button} from "antd";
 import { IFieldEditorView } from "./IFieldEditorView"
+import { IFieldProps } from "../../../models/field.properties";
 
 @observer
 export class FieldPropertiesView extends React.Component<IFieldEditorView, any> {
 
+    isUpdated : boolean = false;
+
     constructor(props: IFieldEditorView) {
         super(props);
+        this.state = toJS(this.props.editorStore.field);
     }
 
-    componentWillUpdate() {
-        console.log("Component will update");
+    setFieldProperty(key:string, value: any) {
+        this.isUpdated = true;
+        this.setState({[key] : value});
+    }
+
+    setComponentProperty(key: string, value: any) {
+        this.isUpdated = true;
+        this.setState({componentProps:{...this.state.componentProps, [key] : value}});
     }
 
     handleSubmit = (e) => {
         e.preventDefault();
         e.stopPropagation();
         let {field} = this.props.editorStore;
-        console.log(`mergeUpdating field with`, this.state);
-        field["mergeUpdate"](this.state.field);
+        field.mergeUpdate(this.state as IFieldProps);
+        this.isUpdated = false;
+        return
     }
 
     render() {
-        let {setFieldProperty, setComponentProperty, field} = this.props.editorStore;
-        let {componentProps} = field;
-
-        return <Row><Col span={20} offset={2}><Form layout="horizontal" onSubmit={(e) => this.handleSubmit(e)}>
+        let field = this.state;
+        return <Row>
+                <Col span={20} offset={2}>
+                <Card title="Update Field Properties">
+                <Form layout="horizontal" onSubmit={(e) => this.handleSubmit(e)}>
                 <Form.Item label="Name" style={{ marginBottom: 10 }} required>
-                    <Input type="text" size="small" placeholder="Field name" value={field.name} onChange={(e) => {setFieldProperty('name', e.target.value)}}></Input>
+                    <Input type="text" placeholder="Field name" value={field.name} onChange={(e) => this.setFieldProperty("name", e.target.value)}></Input>
                 </Form.Item>
                 <Form.Item label="Label" style={{ marginBottom: 10 }}>
-                    <Input type="text" size="small" placeholder="Enter label" value={field.label} onChange={(e) => {setFieldProperty('label', e.target.value)}}></Input>
+                    <Input type="text" placeholder="Enter label" value={field.label} onChange={(e) => {this.setFieldProperty('label', e.target.value)}}></Input>
                 </Form.Item>
                 <Form.Item label="Placeholder Text" style={{ marginBottom: 10 }}>
-                    <Input type="text" size="small" placeholder="Enter placeholder text" value={componentProps["placeholder"]} onChange={(e) => {setComponentProperty('placeholder', e.target.value)}}></Input>
+                    <Input type="text" placeholder="Enter placeholder text" value={field.componentProps.placeholder} onChange={(e) => {this.setComponentProperty('placeholder', e.target.value)}}></Input>
                 </Form.Item>
                 {/* <Form.Item label="Default Value">
                     { field.inputType == 'datepicker' && <DatePicker mode="date"
@@ -42,7 +56,7 @@ export class FieldPropertiesView extends React.Component<IFieldEditorView, any> 
                         format={component.dateFormat} onChange={(m) => setTextValue('componentProps.defaultValue', m.format(component.dateFormat))}/> }
                     { field.inputType == 'number' && <InputNumber value={component.defaultValue} onChange={(e) => {setTextValue('componentProps.defaultValue', e);}}/> }
                     { field.inputType !== 'datepicker' && field.inputType !== 'daterange' &&
-                        <Input type="text" size="small" title="Default Value" placeholder="Enter default value" value={component.defaultValue} onChange={(e) => {setTextValue('componentProps.defaultValue', e.target.value);}}></Input>
+                        <Input type="text" title="Default Value" placeholder="Enter default value" value={component.defaultValue} onChange={(e) => {setTextValue('componentProps.defaultValue', e.target.value);}}></Input>
                     }
                 </Form.Item>
                 { field.inputType ==  'daterange' &&
@@ -56,7 +70,7 @@ export class FieldPropertiesView extends React.Component<IFieldEditorView, any> 
                     </div>
                 } */}
                 {field.inputType.indexOf('checkbox') > 0 && <Form.Item label="Size">
-                    <Select  size="small" value={componentProps["size"] || 'default'} onChange={(e) => {setComponentProperty('size', e)}}>
+                    <Select  value={field.componentProps.size || 'default'} onChange={(e) => {this.setComponentProperty('size', e)}}>
                         <Select.Option value={"default"}>default</Select.Option>
                         <Select.Option value={"small"}>small</Select.Option>
                         <Select.Option value={"large"}>large</Select.Option>
@@ -64,24 +78,30 @@ export class FieldPropertiesView extends React.Component<IFieldEditorView, any> 
                 </Form.Item>}
                 {field.inputType !== 'daterange' &&
                     <Form.Item label="Value property name" required>
-                        <Input type="text" size="small" title="Value property name" placeholder="Value property" value={field.valuePropName} onChange={(e) => {setFieldProperty('valuePropName', e.target.value)}}/>
+                        <Input type="text" title="Value property name" placeholder="Value property" value={field.valuePropName} onChange={(e) => {this.setFieldProperty('valuePropName', e.target.value)}}/>
                     </Form.Item>
                 }
                 {field.inputType == 'daterange' &&
                     <div>
                     <Form.Item label="Start date property name" required>
-                        <Input type="text" size="small" title="Start date value property" placeholder="start_date" value={componentProps["startValuePropsName"]}
-                            onChange={(e) => {e.target.value && e.target.value.length > 0 && setComponentProperty('startValuePropName', e.target.value)}} required/>
+                        <Input type="text" title="Start date value property" placeholder="start_date" value={field.componentProps["startValuePropsName"]}
+                            onChange={(e) => {e.target.value && e.target.value.length > 0 && this.setComponentProperty('startValuePropName', e.target.value)}} required/>
                     </Form.Item>
                     <Form.Item  label="End date property name"  required>
-                        <Input type="text" title="End date value property" placeholder="end_date" value={componentProps["endValuePropsName"]}
-                            onChange={(e) => {e.target.value && e.target.value.length > 0 && setComponentProperty('endValuePropName', e.target.value)}} required/>
+                        <Input type="text" title="End date value property" placeholder="end_date" value={field.componentProps["endValuePropsName"]}
+                            onChange={(e) => {e.target.value && e.target.value.length > 0 && this.setComponentProperty('endValuePropName', e.target.value)}} required/>
                     </Form.Item>
                     </div>
                 }
                 <Form.Item label="Help Text">
-                    <Input.TextArea title="Help Text" placeholder="Enter help text" value={field.helpText} onChange={(e) => {setFieldProperty('helpText', e.target.value)}}></Input.TextArea>
+                    <Input.TextArea title="Help Text" placeholder="Enter help text" value={field.helpText} onChange={(e) => {this.setFieldProperty('helpText', e.target.value)}}></Input.TextArea>
                 </Form.Item>
-            </Form></Col></Row>
+                <Form.Item>
+                    <Button type="primary" htmlType="submit" disabled={!this.isUpdated}>Apply</Button>
+                </Form.Item>
+            </Form>
+            </Card>
+        </Col>
+    </Row>
     }
 }
