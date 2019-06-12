0d5257b2d762fd10cbe94ce6bbf612aadd0371ab
Kartik Rao <kartik.rao@globalred.com.au>
2019-06-12 12:51:09 +1000

    Use defaultValue instead of value

--- src/views/controls/TextBlockView.tsx
+++ src/views/controls/TextBlockView.tsx
@@ -1,6 +1,7 @@
 import { observer } from "mobx-react";
 import * as React from "react";
 import { IViewProps } from "./IViewProps";
+import { IInputProps } from '../../models/field.properties';
 
 @observer
 export class TextBlockView extends React.Component<IViewProps, any> {
@@ -10,6 +11,7 @@ export class TextBlockView extends React.Component<IViewProps, any> {
 
     render() {
         let {field} = this.props;
-        return <p id={field.id} data-uuid={field.uuid}>{field.value}</p>
+        let {defaultValue} = field.componentProps as IInputProps;
+        return <p id={field.id} data-uuid={field.uuid}>{defaultValue}</p>
     }
 }
