03c421c4ea3b88654f589fc05acace7533c72988
Kartik Rao <kartik.rao@adinfinity.com>
2019-06-16 10:40:31 +1000

    Use layout settings for section render

--- src/views/SectionView.tsx
+++ src/views/SectionView.tsx
@@ -26,9 +26,9 @@ export class SectionView extends React.Component<SectionProps, any> {
         let span = numColumns <= 1 ? 24 : 24 / numColumns;
 
         return <div className="fl-section" data-uuid={section.uuid} id={`fl-section-${section.id}`}>
-            <Card bordered={false} title={section.title} style={{padding: "1px"}} size="small">
-                <Row gutter={section.gutter || 4}>
-                    {section.columns.map((column: Column, cn: number) => {
+            <Card bordered={store.form.formLayoutOptions.showSectionBorders} title={store.form.formLayoutOptions.showSectionTitles ? section.title : ""} style={{padding: "1px"}} size="small">
+                <Row gutter={section.gutter}>
+                    {section.columns.map((column: Column) => {
                         return <ColumnView store={store} key={column.uuid} column={column} span={span}/>
                     })}
                 </Row>
