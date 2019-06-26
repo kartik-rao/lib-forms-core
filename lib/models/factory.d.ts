046aacbc7a6fe02db697e4aa26722da8e43229eb
Kartik Rao <kartik.rao@globalred.com.au>
2019-06-26 15:05:07 +1000

    5.0.9

--- lib/models/factory.d.ts
+++ lib/models/factory.d.ts
@@ -11,7 +11,7 @@ import { ISection, Section } from "./section";
 export declare class Factory {
     store: FormStore;
     constructor(store: FormStore);
-    setUUID<T>(item: T): void;
+    ensureIds<T>(item: T): void;
     makePredicates(...predicates: IPredicate[]): Predicate[];
     makeCondition(condition: ICondition): Condition;
     makeFields(...fields: IFieldProps[]): Field[];
