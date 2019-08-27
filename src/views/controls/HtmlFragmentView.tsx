dbab6268b0d6f3464140222f45055604703c7c54
Kartik Rao <kartik.rao@adinfinity.com>
2019-08-27 23:25:20 +1000

    Convert to React.FC

--- src/views/controls/HtmlFragmentView.tsx
+++ src/views/controls/HtmlFragmentView.tsx
@@ -1,19 +1,18 @@
 import * as React from "react";
 import { IHTMLFragmentProps } from '../../models/field.properties';
 import { IViewProps } from './IViewProps';
+import { useObserver } from "mobx-react";
 
-export class HTMLFragmentView extends React.Component<IViewProps, any> {
-    constructor(props: any) {
-        super(props);
-    }
-
-    render() {
-        let {field} = this.props;
-        let {allowForms, allowPopups, allowScripts, fragmentUrl, seamless, ...rest} = field.componentProps as IHTMLFragmentProps;
-        let sandboxProps = [];
-        if (allowForms) {sandboxProps.push("allow-forms")}
-        if (allowPopups) {sandboxProps.push("allow-popups")}
-        if (allowScripts) {sandboxProps.push("allow-scripts")}
-        return <iframe {...rest} src={fragmentUrl} id={field.id} data-uuid={field.uuid} sandbox={sandboxProps.join(" ")} seamless={seamless}></iframe>
-    }
-}
+export const HtmlFragmentView: React.FC<IViewProps> = (props) => {
+    let {field} = props;
+    let {allowForms, allowPopups, allowScripts, fragmentUrl, seamless, ...rest} = field.componentProps as IHTMLFragmentProps;
+    let sandboxProps = [];
+    if (allowForms) {sandboxProps.push("allow-forms")}
+    if (allowPopups) {sandboxProps.push("allow-popups")}
+    if (allowScripts) {sandboxProps.push("allow-scripts")}
+    return useObserver(() => {
+        return <React.Suspense fallback="">
+        <iframe {...rest} src={fragmentUrl} id={field.id} data-uuid={field.uuid} sandbox={sandboxProps.join(" ")} seamless={seamless}></iframe>
+    </React.Suspense>
+    });
+};
