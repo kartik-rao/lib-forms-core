64a137777fdf83883bc71930976a7dc3446c7104
Kartik Rao <kartik.rao@globalred.com.au>
2019-09-03 11:13:09 +1000

    Remove commented code

--- src/views/FormHeaderView.tsx
+++ src/views/FormHeaderView.tsx
@@ -23,34 +23,3 @@ export const FormHeaderView : React.FC<{desc: string, title: string, subtitle: s
         </div>
     });
 }
-// export interface IFormHeaderViewProps {
-//     desc: string;
-//     title: string;
-//     subtitle: string;
-//     showSteps: boolean;
-//     currentPage: number;
-//     numPages: number;
-// }
-
-// @observer
-// export class FormHeaderView extends React.Component<IFormHeaderViewProps, any> {
-//     render() {
-//         let {title, subtitle, desc, currentPage, numPages, showSteps} = this.props;
-
-//         return <div className="fl-shadow-bottom" style={{ marginBottom: '2px'}}>
-//         <PageHeader className="fl-ph" style={{'minHeight': '68px', border: 'none'}} title={title} subTitle={subtitle}
-//             extra={(showSteps && <div>
-//             <Statistic title="Page" value={currentPage + 1} suffix={"/ " + numPages} />
-//             </div>)} />
-//         {title && <Row>
-//             <Col span={24}>
-//                 <div className="fl-ph-wrap">
-//                     <div className="fl-ph-content fl-ph-padding">{desc}</div>
-//                 </div>
-//             </Col>
-//         </Row>
-//         }
-//     </div>
-//     }
-
-// }
