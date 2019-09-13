3c5ce55b4749936962360ed28995437838d1220a
Kartik Rao <kartik.rao@globalred.com.au>
2019-09-13 15:38:00 +1000

    Add debug and refactor submission handling

--- src/views/FormView.tsx
+++ src/views/FormView.tsx
@@ -1,21 +1,38 @@
-import { Button, Card, Col, Form, Icon, Layout, Row } from "antd";
-import { useObserver } from "mobx-react";
+import { Button, Card, Col, Drawer, Form, Icon, Layout, Result, Row } from "antd";
+import { useLocalStore, useObserver } from "mobx-react";
 import * as React from "react";
+import { SubmitState } from '../models/form';
 import { Page } from "../models/page";
 import { formStoreContext } from '../store/FormStoreProvider';
 import { FormHeaderView } from './FormHeaderView';
 import { PageView } from "./PageView";
 
+const DebugView = React.lazy(() => import(/* webpackChunkName: "debug" */ "./DebugView").then((module) => {return {default: module.DebugView}}));
+
 export const FormView : React.FC<any> = () => {
     const fStore = React.useContext(formStoreContext);
     if(!fStore) throw new Error("Store is  null");
 
+    let localStore = useLocalStore(() => ({
+        get showForm() : boolean {
+            return fStore.form.submitState == SubmitState.NOT_SUBMITTED || fStore.form.submitState == SubmitState.SUBMITTING;
+        },
+        get resultStatusTitle() : {status: "success"|"error", title: string} {
+            return {
+                status : fStore.form.submitState == SubmitState.SUCCESS ? "success" : "error",
+                title  : fStore.form.submitState == SubmitState.SUCCESS ? fStore.form.submitSuccessMessage || "Entry submitted successfully" : fStore.form.submitErrorMessage || "There was an error submitting your entry"
+            }
+        }
+    }));
+
     return useObserver(() => {
-        return fStore.form ? <Layout style={{overflow: 'hidden'}} className="fl-form-container fl-max-height">
-                <FormHeaderView title={fStore.form.content.title} subtitle={fStore.form.content.subtitle} desc={fStore.form.description}
-                    currentPage={fStore.currentPage} numPages={fStore.numPages} showSteps={fStore.form.formLayoutOptions.showSteps} />
-                <Layout style={{overflow: 'hidden', border: '1px solid #ededed'}}>
-                    <Form onSubmit={(e) => fStore.form.handleSubmit(e)} layout={fStore.form.layout} className="fl-max-height" wrapperCol={fStore.form.formLayoutOptions.wrapperCol} labelCol={fStore.form.formLayoutOptions.labelCol} labelAlign={fStore.form.formLayoutOptions.labelAlign}>
+        return fStore.form ? <>
+            <Layout style={{overflow: 'hidden', width: fStore.showDebug ? `calc(100% - 500px)` : '100%'}} className="fl-form-container fl-max-height">
+                {localStore.showForm && <FormHeaderView title={fStore.form.content.title} subtitle={fStore.form.content.subtitle} desc={fStore.form.description}
+                    currentPage={fStore.currentPage} numPages={fStore.numPages} showSteps={fStore.form.formLayoutOptions.showSteps} />}
+                <Layout style={{overflow: 'hidden', border: '1px solid #ededed', background: "#ffff"}}>
+                    {fStore.debug && <Drawer title="Debug" width={500} placement="right" mask={false} closable onClose={() => fStore.toggleShowDebug()} visible={fStore.showDebug}><DebugView /></Drawer>}
+                    {localStore.showForm ? <Form onSubmit={(e) => fStore.form.handleSubmit(e)} layout={fStore.form.layout} className="fl-max-height" wrapperCol={fStore.form.formLayoutOptions.wrapperCol} labelCol={fStore.form.formLayoutOptions.labelCol} labelAlign={fStore.form.formLayoutOptions.labelAlign}>
                         <Layout.Content className="fl-form">
                             {fStore.numPages > 0 && <PageView page={fStore.form.content.pages[fStore.currentPage] as Page}></PageView>}
                         </Layout.Content>
@@ -32,8 +49,8 @@ export const FormView : React.FC<any> = () => {
                                 </Card>
                             </div>
                         </Layout.Footer>
-                    </Form>
+                    </Form> : <Result status={localStore.resultStatusTitle.status} title={localStore.resultStatusTitle.title} style={{verticalAlign: "middle"}} subTitle={fStore.debug ? fStore.form.submitError : ""}/>}
                 </Layout>
-        </Layout> : <></>
+        </Layout></> : <></>
     });
 }
