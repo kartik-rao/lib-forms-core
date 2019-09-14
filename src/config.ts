463930a1c6ffae2bf37e509e6f036054a305e0b3
Kartik Rao <kartik.rao@adinfinity.com>
2019-09-14 21:15:29 +1000

    Fix broken undefined checks

--- src/config.ts
+++ src/config.ts
@@ -9,8 +9,8 @@ const port = location.port && location.port.length > 0 ? `:${location.port}` : "
 const host = `${location.hostname}${port}`
 
 export default {
-    env: typeof __ENV__ != undefined ? __ENV__ :env,
-    debug: typeof __DEBUG__ == "undefined" ? __DEBUG__ : (env == "dev" || env == "stage" || location.search.indexOf("$$debug")),
+    env: typeof __ENV__ != "undefined" ? __ENV__ :env,
+    debug: typeof __DEBUG__ != "undefined" ? __DEBUG__ : (env == "dev" || env == "stage" || location.search.indexOf("$$debug")),
     version: typeof __VERSION__ != "undefined" ? __VERSION__ : "6.6.2",
     apiHost : typeof __API_HOST__ != "undefined" ? __API_HOST__ : host
 }
