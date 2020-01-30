d0fef5bada73469d89f7b1af162559782819c957
Kartik Rao <kartik.rao@globalred.com.au>
2020-01-30 16:23:35 +1100

    Updated dist

--- dist/textblock.chunk.js
+++ dist/textblock.chunk.js
@@ -2,5 +2,5 @@
 /*!**********************************************!*\
   !*** ./src/views/controls/TextBlockView.tsx ***!
   \**********************************************/
-/*! exports provided: TextBlockView */function(e,t,o){"use strict";o.r(t),o.d(t,"TextBlockView",function(){return c});var r=o(/*! mobx-react */"./node_modules/mobx-react/dist/mobx-react.module.js"),s=o(/*! react */"react");const c=e=>{let{field:t}=e,{defaultValue:o}=t.componentProps;return Object(r.useObserver)(()=>s.createElement("p",{id:e.field.id,"data-uuid":e.field.uuid},o))}}}]);
+/*! exports provided: TextBlockView */function(e,t,o){"use strict";o.r(t),o.d(t,"TextBlockView",(function(){return c}));var r=o(/*! mobx-react */"./node_modules/mobx-react/dist/mobx-react.module.js"),s=o(/*! react */"react");const c=e=>{let{field:t}=e,{defaultValue:o}=t.componentProps;return Object(r.useObserver)(()=>s.createElement("p",{id:e.field.id,"data-uuid":e.field.uuid},o))}}}]);
 //# sourceMappingURL=textblock.chunk.js.map
